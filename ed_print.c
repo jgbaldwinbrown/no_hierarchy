e30e44e90b5091f2fbe35b9eee1237bf18bc1dfe
James Baldwin-Brown <jgbaldwinbrown@gmail.com>
2020-03-30 11:38:39 -0600

    added ed_print, wrote some sections

---
+++ ed_print.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("ed is the standard editor\n");
+    return(0);
+}
---
+++ ed_print2.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("ed is a good editor\n");
+    return(0);
+}
